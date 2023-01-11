5427fb5c67158014ab61a13291252f970faffc9b
Kirill Kozhevnikov <kkojevnikov1993@gmail.com>
2023-01-12 00:17:43 +0700

    change

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Здравствуйте!");
+
+        int ticket = 19000;
+        int amount = 20;
+
+        int miles = ticket / amount;
+
+        System.out.println(miles);
+
+        System.out.println("Спасибо, что остаетесь с нами!");
+    }
+}
